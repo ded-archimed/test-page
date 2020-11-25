fe74ea204654fe3b126cbbe84aa8874377eafbf7
ded-archimed <69892883+ded-archimed@users.noreply.github.com>
2020-11-25 10:19:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-http_response_code(500);
-//http_response_code(422);
+//http_response_code(500);
+http_response_code(422);
 //http_response_code(401);
 //http_response_code(200);
 //http_response_code(201);
