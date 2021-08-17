7986e747640c3b3e1ff90520de86edd7651833b1
ded-archimed <69892883+ded-archimed@users.noreply.github.com>
2021-08-17 11:45:23 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php
 
-// if(0) {
-// http_response_code(500);
-// }
-// else {
-// http_response_code(422);
-// }
+if(1) {
+http_response_code(500);
+}
+else {
+http_response_code(422);
+}
 
 
 // http_response_code(200);
 // header('Content-Type: application/json');
 // echo json_encode(['status' => 'ok', 'message' => 'success']);
 
-http_response_code(201);
-header('Content-Type: application/json');
-echo json_encode('4c9ec15e52b2c804ab41659dfc9cdfc4');
+// http_response_code(201);
+// header('Content-Type: application/json');
+// echo json_encode('4c9ec15e52b2c804ab41659dfc9cdfc4');
 
 // header("HTTP/1.1 301 Moved Permanently");
 // header("Location: https://test-page-error.herokuapp.com/");
