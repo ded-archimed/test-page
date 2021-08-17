3da666655c938ba64e95bcecc6c4d78bcafa577c
ded-archimed <69892883+ded-archimed@users.noreply.github.com>
2021-08-17 09:55:14 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 
-//http_response_code(500);
+if(0) {
+http_response_code(500);
+}
+else {
 http_response_code(422);
-//http_response_code(401);
-//http_response_code(200);
-//http_response_code(201);
+}
+
+
+// http_response_code(200);
+// header('Content-Type: application/json');
+// echo json_encode(['status' => 'ok', 'message' => 'success']);
+
+// http_response_code(201);
+// header('Content-Type: application/json');
+// echo json_encode('4fdea8e75f646bb83a34ee1757850c3e');
